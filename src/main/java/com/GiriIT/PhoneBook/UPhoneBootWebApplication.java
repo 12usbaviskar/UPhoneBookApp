ddf0b301fd202af2a69d56bc70b8604a3006aa5e
unknown <umeshbaviskarus12@gmail.comgit config --global user.name 12usbaviskar>
2022-06-14 00:36:18 +0530

    PHON-4: TASK COMPLTE

---
+++ src/main/java/com/GiriIT/PhoneBook/UPhoneBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.GiriIT.PhoneBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UPhoneBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UPhoneBootWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GiriIT/PhoneBook/UPhoneBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GiriIT.PhoneBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UPhoneBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
